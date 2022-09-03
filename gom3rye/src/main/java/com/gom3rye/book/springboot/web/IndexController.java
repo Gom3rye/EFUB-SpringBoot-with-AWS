9bdfd4c5c42ef6b6892a02356589f9dfca5552fe
Gom3rye <kimmyeongji3@gmail.com>
2022-09-03 21:44:58 +0900

    [5장] 구글 로그인 구현하기(@LoginUser로 클린 코드 구현)

--- gom3rye/src/main/java/com/gom3rye/book/springboot/web/IndexController.java
+++ gom3rye/src/main/java/com/gom3rye/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.gom3rye.book.springboot.web;
 
+import com.gom3rye.book.springboot.config.auth.LoginUser;
+import com.gom3rye.book.springboot.config.auth.dto.SessionUser;
 import com.gom3rye.book.springboot.service.posts.PostsService;
 import com.gom3rye.book.springboot.web.dto.PostsResponseDto;
 import lombok.Getter;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -16,8 +20,11 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){  // 기존에 (User) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선되었다. 이젠 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 됐다.
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) { // 세션에 저장된 값이 있을 때만 model에 userName으로 등록한다.
+            model.addAttribute("loginUserName", user.getName());
+        }
         return "index";
     }
     @GetMapping("/posts/save") // 머스테치로 /posts/save 페이지 주소 만들어주었으니까 이 주소에 해당하는 컨트롤러 생성
@@ -26,7 +33,7 @@ public class IndexController {
         return "posts-save";
     }
 
-    @GetMapping("/posts/update{id}")
+    @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model)
     {
         PostsResponseDto dto = postsService.findById(id);
